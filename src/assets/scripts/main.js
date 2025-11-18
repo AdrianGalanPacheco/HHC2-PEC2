cf84ff48d36c51c6fd1334b3cb803d1e064157a4
adrian galan pacheco <agalanpa@uoc.edu>
2025-11-18 12:33:28 +0100

    PEC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,27 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import * as bootstrap from "bootstrap";
+import flatpickr from "flatpickr";
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+flatpickr("#datepicker", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+});
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
